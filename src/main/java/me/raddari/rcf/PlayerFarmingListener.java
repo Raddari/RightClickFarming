79f549cae3c9162123824d94fd7cd61f7bd01508
Harvey <harveywyatt@hotmail.com>
2020-07-20 19:44:45 +0930

    Add PlayerFarmingListener

---
+++ src/main/java/me/raddari/rcf/PlayerFarmingListener.java
@@ -0,0 +1,14 @@
+package me.raddari.rcf;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public final class PlayerFarmingListener implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+
+    }
+
+}
