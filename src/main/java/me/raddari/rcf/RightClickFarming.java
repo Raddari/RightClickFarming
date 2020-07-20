fa279675723588f1bbf279d33bcdf836effa4962
Harvey <harveywyatt@hotmail.com>
2020-07-20 19:30:38 +0930

    Initial commit

---
+++ src/main/java/me/raddari/rcf/RightClickFarming.java
@@ -0,0 +1,11 @@
+package me.raddari.rcf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RightClickFarming extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Enable stuff
+    }
+}
